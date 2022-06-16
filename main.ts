b5394fdadc109671565121e09248ad13cfb05d85
yene27 <yene27@yahoo.com>
2022-06-15 21:22:10 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,31 @@
 let numero1 = 0
 let numero2 = 0
+input.onGesture(Gesture.LogoUp, function () {
+    numero1 = randint(1, 10)
+    basic.showNumber(numero1)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        # # # # #
+        . . . . .
+        . . # . .
+        `)
+    numero2 = randint(1, 10)
+    basic.showNumber(numero2)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showNumber(numero1 / numero2)
+    basic.pause(1000)
+    basic.clearScreen()
+})
 input.onButtonPressed(Button.A, function () {
     numero1 = randint(1, 10)
     basic.showNumber(numero1)
