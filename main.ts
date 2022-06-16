34750f4d630ccd558b2c279b1e5a83392b099a6b
yene27 <yene27@yahoo.com>
2022-06-15 21:18:06 -0500

    suma, resta y multiplicacion

--- main.ts
+++ main.ts
@@ -26,6 +26,32 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(1000)
     basic.clearScreen()
 })
+input.onGesture(Gesture.Shake, function () {
+    numero1 = randint(1, 10)
+    basic.showNumber(numero1)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    numero2 = randint(1, 10)
+    basic.showNumber(numero2)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showNumber(numero1 * numero2)
+    basic.pause(1000)
+    basic.clearScreen()
+})
 input.onButtonPressed(Button.B, function () {
     numero1 = randint(1, 10)
     basic.showNumber(numero1)
